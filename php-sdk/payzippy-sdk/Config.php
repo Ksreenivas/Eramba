815b6a9ad8467605a7965d388ab41c2560fa2764
sreenivas1591@gmail.com <sreenivas1591@gmail.com>
2014-01-07 12:34:35 +0530

    payment gateway
    PayZippy

--- php-sdk/payzippy-sdk/Config.php
+++ php-sdk/payzippy-sdk/Config.php
@@ -2,14 +2,14 @@
 
 final class PZ_Config
 {
-    const MERCHANT_ID = ""; // Your Merchant ID
-    const SECRET_KEY = ""; // Your Secret Key. Do not share this.
+    const MERCHANT_ID = "test_t494"; // Your Merchant ID
+    const SECRET_KEY = "dca056eb3ccf5f487be49b67fa5aa68f33a2b94b8160e4aebf55594a36aad6f0"; // Your Secret Key. Do not share this.
     const TRANSACTION_TYPE = "SALE";
     const CURRENCY = "INR";
     const UI_MODE = "REDIRECT"; // UI Integration - REDIRECT or IFRAME
     const HASH_METHOD = "MD5"; // MD5 or SHA256
-    const MERCHANT_KEY_ID = ""; // Your Merchant Key ID
-    const CALLBACK_URL = ""; // Your callback URL
+    const MERCHANT_KEY_ID = "payment"; // Your Merchant Key ID
+    const CALLBACK_URL = "http://192.168.33.10/php-sdk/examples/response/charging_response.php"; // Your callback URL
 
     const API_BASE = "https://www.payzippy.com/payment/api/";
     const API_CHARGING = "charging";
