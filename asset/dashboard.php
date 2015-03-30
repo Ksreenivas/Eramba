e7263961958d23c481fa1897eff29764daabc7e8
sreenivas <sreenivas1591@gmail.com>
2015-03-30 17:52:09 +0530

    Test Notification email 3

--- asset/dashboard.php
+++ asset/dashboard.php
@@ -25,7 +25,36 @@ $asset_chart_data = list_dashboard_chart("asset_dashboard_tbl");
 						    ]);
 						  
 						    // Create and draw the visualization.
-						    new google.visualization.LineChart(document.getElementById('visualization')).
+						    new google.visualization.itLineChart(document.getElementById('visualization')).
+						        draw(data, {curveType: "function",
+						                    width: window_width, height: 200,
+						                    vAxis: {maxValue: 10},
+									fontSize: 11
+								}
+						            );
+						  }
+
+						  google.setOnLoadCallback(drawVisualization);
+
+						</script>ss="widget">
+					<div class="widget-header">Number of assets per type</div>
+					<div class="widget-content">
+
+						<script type="text/javascript">
+						var window_width = $(window).width() - 100;
+						  function drawVisualization() {
+						    // Create and populate the data table.
+						    var data = google.visualization.arrayToDataTable([
+						      ['x',         'Total # of Assets', 'Data Assets', 'Hardware', 'Human', 'Information Systems', 'Software'],
+						      <?php
+						      	foreach ( $asset_chart_data as $data ) : ?>
+						      		<?php $date = date( "m-Y", strtotime($data["dashboard_date"]) ); ?>
+						      		['<?php echo $date ?>', <?php echo $data['count_assets_total']; ?>, <?php echo $data['count_assets_type_one']; ?>,<?php echo $data['count_assets_type_two']; ?>,<?php echo $data['count_assets_type_three']; ?>,<?php echo $data['count_assets_type_four']; ?>,<?php echo $data['count_assets_type_five']; ?>], 
+						      	<?php endforeach; ?>
+						    ]);
+						  
+						    // Create and draw the visualization.
+						    new google.visualization.itLineChart(document.getElementById('visualization')).
 						        draw(data, {curveType: "function",
 						                    width: window_width, height: 200,
 						                    vAxis: {maxValue: 10},
