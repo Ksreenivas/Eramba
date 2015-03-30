52386a79c5b020ba9f121e5122a007fdcf319970
sreenivas <sreenivas1591@gmail.com>
2015-03-30 17:44:05 +0530

    Email Notification Test 2

--- asset/asset_classification_list.php
+++ asset/asset_classification_list.php
@@ -18,16 +18,6 @@
 	$section = isset($_GET["section"])?$_GET["section"]:null;
 	$subsection = isset($_GET["subsection"])?$_GET["subsection"]:null;
 	$action = isset($_GET["action"])?$_GET["action"]:null;
-	$action = isset($_GET["action"])?$_GET["action"]:null;
-	
-	$base_url_edit = build_base_url($section,"asset_classification_edit");
-	$base_url_list = bui
-	ld_base_url($section,"asset_classification_list");
-	$show_id = isset($_GET["show_id"]) ? $_GET["show_id"] : null;
-	$sort = isset($_GET["sort"])?$_GET["sort"]:null;
-	$section = isset($_GET["section"])?$_GET["section"]:null;
-	$subsection = isset($_GET["subsection"])?$_GET["subsection"]:null;
-	$action = isset($_GET["action"])?$_GET["action"]:null;
 	
 	$base_url_edit = build_base_url($section,"asset_classification_edit");
 	$base_url_list = build_base_url($section,"asset_classification_list");
