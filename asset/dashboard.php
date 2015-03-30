cdb57d5bf3f25b5e554cd7d974799bda385933aa
sreenivas <sreenivas1591@gmail.com>
2015-03-30 18:11:04 +0530

    Test mail

--- asset/dashboard.php
+++ asset/dashboard.php
@@ -36,35 +36,22 @@ $asset_chart_data = list_dashboard_chart("asset_dashboard_tbl");
 
 						  google.setOnLoadCallback(drawVisualization);
 
-						</script>ss="widget">
-					<div class="widget-header">Number of assets per type</div>
+						</script>
+
+							<div id="visualization" style="height: 200px;"></div>
+							<p></p>
+
+					</div>
+				</div>
+				<div class="widget">
+					<div class="widget-header">Data Asset Flows Analysis - Warnings by Percentage</div>
 					<div class="widget-content">
 
 						<script type="text/javascript">
 						var window_width = $(window).width() - 100;
 						  function drawVisualization() {
 						    // Create and populate the data table.
-						    var data = google.visualization.arrayToDataTable([
-						      ['x',         'Total # of Assets', 'Data Assets', 'Hardware', 'Human', 'Information Systems', 'Software'],
-						      <?php
-						      	foreach ( $asset_chart_data as $data ) : ?>
-						      		<?php $date = date( "m-Y", strtotime($data["dashboard_date"]) ); ?>
-						      		['<?php echo $date ?>', <?php echo $data['count_assets_total']; ?>, <?php echo $data['count_assets_type_one']; ?>,<?php echo $data['count_assets_type_two']; ?>,<?php echo $data['count_assets_type_three']; ?>,<?php echo $data['count_assets_type_four']; ?>,<?php echo $data['count_assets_type_five']; ?>], 
-						      	<?php endforeach; ?>
-						    ]);
-						  
-						    // Create and draw the visualization.
-						    new google.visualization.itLineChart(document.getElementById('visualization')).
-						        draw(data, {curveType: "function",
-						                    width: window_width, height: 200,
-						                    vAxis: {maxValue: 10},
-									fontSize: 11
-								}
-						            );
-						  }
-
-						  google.setOnLoadCallback(drawVisualization);
-
+						    var dat
 						</script>
 
 							<div id="visualization" style="height: 200px;"></div>
